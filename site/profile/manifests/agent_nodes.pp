0abb1d68985fbc76ce2b0f17c06b074924c74efc
Sviatoslav Solovei <51817503+swatkiev@users.noreply.github.com>
2022-08-15 14:35:28 +0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,5 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.docker.node':}
   dockeragent::node {'db.docker.node':}
+  dockeragent::node {'minetest.docker.node':}
 }
