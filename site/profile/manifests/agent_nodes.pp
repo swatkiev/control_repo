cc67cfdd742b49f5d9650f9577733c5a8c0a7d08
Sviatoslav Solovei <51817503+swatkiev@users.noreply.github.com>
2022-08-15 18:14:14 +0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,4 +3,12 @@ class profile::agent_nodes {
   dockeragent::node {'web.docker.node':}
   dockeragent::node {'db.docker.node':}
   dockeragent::node {'minetest.docker.node':}
+  host {'web.docker.node':
+    ensure => present,
+    ip => 'put here ip address of web.docker.node'
+  }
+  host {'db.docker.node':
+    ensure => present,
+    ip => 'put here ip address of db.docker.node'
+  }
 }
